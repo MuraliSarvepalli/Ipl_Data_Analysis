63532b35a3d446a933d5b813574cf0702bd9dedf
MuraliSarvepalli <43774671+MuraliSarvepalli@users.noreply.github.com>
2024-08-17 23:23:09 +0530

    Update project_analysis.sql

--- project_analysis.sql
+++ project_analysis.sql
@@ -37,6 +37,30 @@ select winner,count(*) as no_of_wins from murali_ipl.matches_raw
 group by winner order by count(*) desc ;
 
 
+------5. Batsman with most not outs in successful chases in IPL----- 
+
+with cte as (
+  select match_id , max(over_no) as over_no from murali_ipl.deliveries_raw
+  where inning=2
+  group by match_id
+),
+cte2 as (
+select b.* ,row_number() over(partition by b.match_id order by b.ball desc)  as rn from cte a inner join murali_ipl.deliveries_raw b
+on a.match_id = b.match_id and a.over_no = b.over_no
+where b.inning = 2 and b.is_wicket = 0-- and b.match_id =1254094
+)
+select batter , sum(no_of_not_outs_in_chasing) as no_of_not_outs_in_chasing from (
+select batter ,count(*) as no_of_not_outs_in_chasing from cte2 a inner join murali_ipl.matches_raw 
+b on a.match_id = b.id 
+and a.batting_team = b.winner  where a.rn = 1
+group by batter
+union all 
+select non_striker as batter ,count(*) as no_of_not_outs_in_chasing from cte2 a inner join murali_ipl.matches_raw 
+b on a.match_id = b.id 
+and a.batting_team = b.winner  where a.rn = 1
+group by batter)a group by batter order by no_of_not_outs_in_chasing desc;
+
+
 
 
 
